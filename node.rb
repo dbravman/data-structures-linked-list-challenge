d1a9f8de1dffa67154a436db628d91458ef6354c
Danny Bravman <dbravman@gmail.com>
2016-07-17 21:43:40 -0500

    new, insert after, remove after

--- node.rb
+++ node.rb
@@ -1,3 +1,14 @@
 class Node
+  def initialize(element)
+    @value = element
+    @next = nil
+  end
 
+  def insert_after(node)
+    @next = node
+  end
+
+  def remove_after
+    @next = nil
+  end
 end
--- node_spec.rb
+++ node_spec.rb
@@ -1,5 +1,7 @@
 require_relative 'node'
 
 describe Node do
+  let(:node) {Node.new(0)}
 
+  it "has value"
 end
