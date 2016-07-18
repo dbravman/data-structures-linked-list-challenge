6f56ab6185fd8b3efda4fcacf643d15fb1662eac
Danny Bravman <dbravman@gmail.com>
2016-07-18 14:56:02 -0500

    release 1 methods

--- linked_list.rb
+++ linked_list.rb
@@ -2,4 +2,36 @@ require_relative 'node'
 
 class LinkedList
 
+  def initialize
+    @first_node = nil
+  end
+
+  def insert_first(element)
+    new_node = Node.new(element)
+    new_node.insert_after(@first_node)
+    @first_node = new_node  
+  end
+
+  def remove_first
+    return nil if @first_node == nil
+    @first_node = @first_node.next
+  end
+
+  def insert_last(element)
+    new_node = Node.new(element)
+    current = @first_node 
+    until current.next == nil
+      current = @first_node.next
+    end
+    current.insert_after(new_node)
+  end
+
+  def remove_last
+    return nil if @first_node == nil
+    current = @first_node
+    until current.next.next == nil
+      current = @first_node.next
+    end
+    current.remove_after
+  end
 end
